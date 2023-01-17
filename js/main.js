e1b7cc2d6c36a5b6cc6c09f96573993cd72e56a4
aaz <aaz161@yandex.ru>
2023-01-17 18:44:16 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+
+const scene = document.getElementById('scene');
+const scene2 = document.getElementById('scene2');
+const parallaxInstance = new Parallax(scene);
+const parallaxInstance2 = new Parallax(scene2);
