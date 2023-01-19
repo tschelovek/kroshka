084b451073e646bc4d73045ece7c04e1b16bb540
aaz <aaz161@yandex.ru>
2023-01-19 12:08:46 +0300

    without adaptive

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 
 const scene = document.getElementById('scene');
 const scene2 = document.getElementById('scene2');
+const scene3 = document.getElementById('scene3');
+const scene4 = document.getElementById('scene4');
 const parallaxInstance = new Parallax(scene);
 const parallaxInstance2 = new Parallax(scene2);
+const parallaxInstance3 = new Parallax(scene3);
+const parallaxInstance4 = new Parallax(scene4);
+
+AOS.init();
