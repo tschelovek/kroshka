c0c986300467db1212b4edcc21f72101d0b1b72e
tschelovek <az18rg@gmail.com>
2023-01-22 20:29:37 +0300

    ready ver 1.0.0

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-
-// Паралакс
-const scene1 = document.getElementById('scene1');
-const scene2 = document.getElementById('scene2');
-const scene3 = document.getElementById('scene3');
-const scene4 = document.getElementById('scene4');
-const scene5 = document.getElementById('scene5');
-const scene6 = document.getElementById('scene6');
-const parallaxInstance1 = new Parallax(scene1);
-const parallaxInstance2 = new Parallax(scene2);
-const parallaxInstance3 = new Parallax(scene3);
-const parallaxInstance4 = new Parallax(scene4);
-const parallaxInstance5 = new Parallax(scene5);
-const parallaxInstance6 = new Parallax(scene6);
+document.addEventListener('DOMContentLoaded', () => {
+    // Паралакс
+    // const scene1 = document.getElementById('scene1');
+    // const scene2 = document.getElementById('scene2');
+    // const scene3 = document.getElementById('scene3');
+    // const scene4 = document.getElementById('scene4');
+    // const scene5 = document.getElementById('scene5');
+    // const scene6 = document.getElementById('scene6');
+    const parallaxInstance1 = new Parallax(document.getElementById('scene1'));
+    const parallaxInstance2 = new Parallax(document.getElementById('scene2'));
+    const parallaxInstance3 = new Parallax(document.getElementById('scene3'));
+    const parallaxInstance4 = new Parallax(document.getElementById('scene4'));
+    const parallaxInstance5 = new Parallax(document.getElementById('scene5'));
+    const parallaxInstance6 = new Parallax(document.getElementById('scene6'));
 
 // Появление элементов при прокрутке страныцы
-AOS.init();
+    AOS.init();
+
+    // HAMBURGER
+
+    const burgerButton = document.getElementById('burgerButton');
+    if (burgerButton) {
+        burgerButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            burgerButton.classList.toggle('active');
+            document.getElementById('burgerMenu').classList.toggle('active');
+        })
+    }
+})
