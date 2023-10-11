5c13ee22794e0f5771aad4a1899e11dd58c68e05
tschelovek <az18rg@gmail.com>
2023-10-11 19:55:44 +0300

    ver. 1.0.3 fixes

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Паралакс
-    // const scene1 = document.getElementById('scene1');
-    // const scene2 = document.getElementById('scene2');
-    // const scene3 = document.getElementById('scene3');
-    // const scene4 = document.getElementById('scene4');
-    // const scene5 = document.getElementById('scene5');
-    // const scene6 = document.getElementById('scene6');
     const parallaxInstance1 = new Parallax(document.getElementById('scene1'));
     const parallaxInstance2 = new Parallax(document.getElementById('scene2'));
     const parallaxInstance3 = new Parallax(document.getElementById('scene3'));
